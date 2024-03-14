58e62c125bfb9b884a613da43f8f6e92a761f433
wankhadeayushi5@gmail <wankhadeayushi@gmail.com>
2024-03-14 16:18:14 +0530

    Practical sixth completed

---
+++ PracticalNoB.java
@@ -0,0 +1,13 @@
+
+// File: AnotherClass.java
+import learnjava.PracticalNo6A;
+
+public class PracticalNo6B {
+    public static void main(String[] args) {
+        // Creating an instance of the First class
+        PracticalNo6A firstObject = new PracticalNo6A();
+
+        // Accessing the display method of the First class
+        firstObject.display();
+    }
+}
